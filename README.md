eb1105067177df7d36decb99c994b179f3c2dd9c
xukaizhong <xukaizhong@jd.com>
2020-08-10 21:54:39 +0800

    ** 插入排序

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ likou.August.AugustServer
 ## 二、排序算法
 ### 1、选择排序
 likou.August.AugustTen
+### 2、插入排序
+likou.August.AugustTen
--- src/main/java/likou/August/AugustTen.java
+++ src/main/java/likou/August/AugustTen.java
@@ -31,9 +31,36 @@ public class AugustTen {
         return numbs;
     }
 
+    /**
+     * 插入排序
+     * @param numbs
+     * @return
+     */
+    private static int[] sort2(int[] numbs) {
+        for (int i = 0;i<numbs.length;i++){
+            int tar = i;
+            //比较大小，找到最小值
+            for (int j = i-1 ;j>0;j--){
+                //如果目标值比当前值小交换位置
+                if (numbs[tar] <= numbs[j]){
+                    //将最小值min和当前位置进行交换j
+                    int tem = numbs[tar];
+                    numbs[tar] = numbs[j];
+                    numbs[j] = tem;
+                }else {
+                    break;
+                }
+                tar = j;
+            }
+
+
+        }
+        return numbs;
+    }
+
 
     public static void main(String[] args) {
-        int[] a = sort(new int[]{1,3,2,54,53,1,342,35,675,89,9,23});
+        int[] a = sort2(new int[]{1,3,2,54,53,1,342,35,675,89,9,23});
 
         for (int i = 0;i<a.length;i++){
             System.out.println(a[i]);
