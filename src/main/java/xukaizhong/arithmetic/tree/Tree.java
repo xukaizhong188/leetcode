324757cfd6ba0ffc2c11225cb713ff3ddccb30e1
xukaizhong <xukaizhong@jd.com>
2021-03-21 17:20:15 +0800

    ** 树

--- src/main/java/xukaizhong/arithmetic/tree/Tree.java
+++ src/main/java/xukaizhong/arithmetic/tree/Tree.java
@@ -73,6 +73,136 @@ public class Tree {
         deleNode(rootTreeNode);
     }
 
+    /**
+     * 获取树的深度
+     * @return
+     */
+    private int getTreeHight(){
+        return getHight(rootTreeNode);
+    }
+
+    /**
+     * 获取某个接节点的度
+     * @param treeNode
+     * @return
+     */
+    private int getHight(TreeNode treeNode){
+        if (null == treeNode){
+            return 0;
+        }
+        int rightHight = getHight(treeNode.getRightNode());
+        int leftHight = getHight(treeNode.getLeftNode());
+        int max = Math.max(rightHight,leftHight);
+        return max+1;
+    }
+
+    /**
+     * 获取二叉树的节点数
+     * @return
+     */
+    private int getTreeNum(){
+        return getNodeNum(rootTreeNode);
+    }
+
+    /**
+     * 获取指定节点的节点数
+     * @param treeNode
+     * @return
+     */
+    private int getNodeNum(TreeNode treeNode){
+        if (null == treeNode){
+            return 0;
+        }
+        int leftNodeNum = getNodeNum(treeNode.getLeftNode());
+        int rightNodeNum = getNodeNum(treeNode.getRightNode());
+        return leftNodeNum +rightNodeNum+1;
+    }
+
+    /**
+     * 获取指定节点的父节点
+     * @param treeNode
+     * @return
+     */
+    private TreeNode getParentNode(TreeNode treeNode){
+
+        if (null == rootTreeNode || treeNode == rootTreeNode){
+            return null;
+        }
+        return getParentNode(treeNode,rootTreeNode);
+    }
+
+    /**
+     * 递归遍历子节点和目标节点是否一致
+     * @param targetNode
+     * @param childNode
+     * @return
+     */
+    private TreeNode getParentNode(TreeNode targetNode,TreeNode childNode){
+        if (null == childNode){
+            return null;
+        }
+        if (childNode.getLeftNode() == targetNode || childNode.getRightNode() == targetNode){
+            return childNode;
+        }
+        if (null != getParentNode(targetNode,childNode.getLeftNode())) {
+            return getParentNode(targetNode,childNode.getLeftNode());
+        }else {
+            return getParentNode(targetNode,childNode.getRightNode());
+        }
+    }
+
+    /**
+     * 打印节点值
+     * @param treeNode
+     */
+    private void op(TreeNode treeNode){
+        if (null != treeNode){
+            System.out.println(treeNode.getValue());
+        }
+    }
+
+    /**
+     * 先序遍历 根 左 右
+     * @param treeNode
+     */
+    private void iterateFirstOrder(TreeNode treeNode){
+        if (null == treeNode){
+            return;
+        }
+        op(treeNode);
+        insertLeftTreeNode(treeNode.getLeftNode());
+        insertLeftTreeNode(treeNode.getRightNode());
+    }
+
+    /**
+     * 中序遍历 左 根 右
+     * @param treeNode
+     */
+    private void iterateMediumOrder(TreeNode treeNode){
+        if (null == treeNode){
+            return;
+        }
+
+        iterateMediumOrder(treeNode.getLeftNode());
+        op(treeNode);
+        iterateMediumOrder(treeNode.getRightNode());
+    }
+
+    /**
+     * 后序遍历  左右根
+     * @param treeNode
+     */
+    private void iterateLastOrder(TreeNode treeNode){
+        if (null == treeNode){
+            return;
+        }
+
+        iterateMediumOrder(treeNode.getLeftNode());
+        iterateMediumOrder(treeNode.getRightNode());
+        op(treeNode);
+    }
+
+
     public static void main(String[] args) {
 
     }
